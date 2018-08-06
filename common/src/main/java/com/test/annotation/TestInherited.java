53c60867d4fd3930fb615a85e326b622601678ac
dasouche1 <shenfeilong@souche.com>
2018-08-06 19:24:38 +0800

    shenfeilong add annotation

--- common/src/main/java/com/test/annotation/TestInherited.java
+++ common/src/main/java/com/test/annotation/TestInherited.java
@@ -3,6 +3,7 @@ package com.test.annotation;
 /**
  * Created by shenfl on 2018/8/6
  * 当InheritedAnnotation有Inherited注解时，B也有InheritedAnnotation注解，反之没有
+ * A是类和抽象类都行，但是如果A是接口则不行
  */
 public class TestInherited {
     public static void main(String[] args) {
@@ -10,7 +11,7 @@ public class TestInherited {
         System.out.println(glass.isAnnotationPresent(InheritedAnnotation.class));
     }
     @InheritedAnnotation
-    static class A {
+    static abstract class A {
 
     }
     static class B extends A {

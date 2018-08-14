defbdcbf9faf746ef4daf27b6bdee110fa5fbb0f
dasouche1 <shenfeilong@souche.com>
2018-08-14 16:11:25 +0800

    shenfeilong add reflect test

---
+++ spring/src/main/java/com/test/java8/TestReflect.java
@@ -0,0 +1,24 @@
+package com.test.java8;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by shenfl on 2018/8/14
+ * 测试反射当调用15次以下是NativeMethodAccessorImpl，较慢，当超过15次后使用GeneratedMethodAccessor1，会动态生成字节码运行
+ */
+public class TestReflect {
+    // 如果是private方法，通过正常的反射拿不到
+    public static void print(int i) {
+        new Exception("" + i).printStackTrace();
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> aClass = Class.forName("com.test.java8.TestReflect");
+        Method print = aClass.getMethod("print", int.class);
+        print.setAccessible(true);
+        for (int i = 0; i < 20; i++) {
+            print.invoke(null, i);
+        }
+    }
+}

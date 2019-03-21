80938176cfca3ee6cc890f3889fa0081f45a2b58
shenfl <shenfeilong@souche.com>
2019-03-21 11:13:14 +0800

    shenfeilong add hadoop input format test

---
+++ eshadoop/src/main/java/com/test/es/hadoop/SmallFilesToSequenceFileConverter.java
@@ -0,0 +1,68 @@
+package com.test.es.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+/**
+ * 小文件变成序列化文件
+ * 另一种方式的mapreduce写法
+ */
+public class SmallFilesToSequenceFileConverter {
+    static class SequenceFileMapper extends Mapper<NullWritable, BytesWritable, Text, BytesWritable> {
+        private Text filenameKey;
+
+        @Override
+        protected void setup(Context context) throws IOException,
+                InterruptedException {
+            InputSplit split = context.getInputSplit();
+            Path path = ((FileSplit) split).getPath();
+            filenameKey = new Text(path.toString());
+        }
+
+        @Override
+        protected void map(NullWritable key, BytesWritable value,
+                           Context context) throws IOException, InterruptedException {
+            context.write(filenameKey, value);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        args=new String[]{"c:/wordcount/smallinput","c:/wordcount/smallout"};
+        Configuration conf = new Configuration();
+        /*System.setProperty("HADOOP_USER_NAME", "hadoop");*/
+        String[] otherArgs = new GenericOptionsParser(conf, args)
+                .getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: combinefiles <in> <out>");
+            System.exit(2);
+        }
+
+        Job job = Job.getInstance(conf,"combine small files to sequencefile");
+        job.setJarByClass(SmallFilesToSequenceFileConverter.class);
+
+        job.setInputFormatClass(WholeFileInputFormat.class);
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(BytesWritable.class);
+        job.setMapperClass(SequenceFileMapper.class);
+
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+
+    }
+}
---
+++ eshadoop/src/main/java/com/test/es/hadoop/WholeFileRecordReader.java
@@ -0,0 +1,87 @@
+package com.test.es.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+/**
+ *
+ * RecordReader的核心工作逻辑：
+ * 通过nextKeyValue()方法去读取数据构造将返回的key   value
+ * 通过getCurrentKey 和 getCurrentValue来返回上面构造好的key和value
+ *
+ *
+ * @author
+ *
+ */
+class WholeFileRecordReader extends RecordReader<NullWritable, BytesWritable> {
+    private FileSplit fileSplit;
+    private Configuration conf;
+    private BytesWritable value = new BytesWritable();
+    private boolean processed = false;
+
+    @Override
+    public void initialize(InputSplit split, TaskAttemptContext context)
+            throws IOException, InterruptedException {
+        this.fileSplit = (FileSplit) split;
+        this.conf = context.getConfiguration();
+    }
+
+    @Override
+    public boolean nextKeyValue() throws IOException, InterruptedException {
+        if (!processed) {
+            byte[] contents = new byte[(int) fileSplit.getLength()];
+            Path file = fileSplit.getPath();
+            FileSystem fs = file.getFileSystem(conf);
+            FSDataInputStream in = null;
+            try {
+                in = fs.open(file);
+                IOUtils.readFully(in, contents, 0, contents.length);
+                value.set(contents, 0, contents.length);
+            } finally {
+                IOUtils.closeStream(in);
+            }
+            processed = true;
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    @Override
+    public NullWritable getCurrentKey() throws IOException,
+            InterruptedException {
+        return NullWritable.get();
+    }
+
+    @Override
+    public BytesWritable getCurrentValue() throws IOException,
+            InterruptedException {
+        return value;
+    }
+
+    /**
+     * 返回当前进度
+     */
+    @Override
+    public float getProgress() throws IOException {
+        return processed ? 1.0f : 0.0f;
+    }
+
+    @Override
+    public void close() throws IOException {
+        // do nothing
+    }
+}

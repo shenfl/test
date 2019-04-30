abc46217576c3ba372f0af35d4f856f06f0d3cff
shenfl <shenfeilong@souche.com>
2019-04-30 14:59:15 +0800

    shenfeilong add thread pool test

---
+++ common/src/main/java/com/test/java/TestThread.java
@@ -0,0 +1,30 @@
+package com.test.java;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TestThread {
+    public static void main(String[] args) {
+        ExecutorService executorService = new ThreadPoolExecutor(3, 3, 10, TimeUnit.SECONDS, new LinkedBlockingDeque<>(10));
+        /**
+         * 当为14时抛出异常RejectedExecutionException
+         */
+        for (int i = 0; i < 14; i++) {
+            int finalI = i;
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(finalI + " haha");
+                    try {
+                        Thread.sleep(1000000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(finalI + " over");
+                }
+            });
+        }
+    }
+}

1507f8b4bf23b37c525ee38493295452860c6f3c
shenfl <shenfeilong@souche.com>
2020-04-10 10:43:11 +0800

    shenfeilong test thread pool

--- common/src/main/java/com/test/java/TestThread.java
+++ common/src/main/java/com/test/java/TestThread.java
@@ -7,12 +7,13 @@ import java.util.concurrent.TimeUnit;
 
 public class TestThread {
     public static void main(String[] args) {
-        ExecutorService executorService = new ThreadPoolExecutor(3, 3, 10, TimeUnit.SECONDS, new LinkedBlockingDeque<>(10));
+        ExecutorService executorService = new ThreadPoolExecutor(3, 5, 10, TimeUnit.SECONDS, new LinkedBlockingDeque<>(10));
         /**
          * 当为14时抛出异常RejectedExecutionException
          */
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < 20; i++) {
             int finalI = i;
+            System.out.println("submit " + i);
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {

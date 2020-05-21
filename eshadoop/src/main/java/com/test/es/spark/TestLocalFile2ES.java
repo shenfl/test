fcc4ed3a217043c968314fee5a44e779fd84dc46
shenfl <shenfeilong@souche.com>
2020-05-21 17:45:06 +0800

    shenfeilong change es-hadoop test

--- eshadoop/src/main/java/com/test/es/spark/TestLocalFile2ES.java
+++ eshadoop/src/main/java/com/test/es/spark/TestLocalFile2ES.java
@@ -1,16 +1,17 @@
-//package com.test.es.spark;
-//
-//import org.apache.spark.sql.Dataset;
-//import org.apache.spark.sql.Row;
-//import org.apache.spark.sql.SparkSession;
-//import org.elasticsearch.spark.sql.api.java.JavaEsSparkSQL;
-//
-//public class TestLocalFile2ES {
-//    public static void main(String[] args) {
-//        SparkSession spark=SparkSession.builder().appName("Sql2Es").config("es.nodes","172.17.40.129")
-//                .config("es.port","9200").master("local[3]").getOrCreate();
-//        Dataset<Row> person=spark.read().json("/Users/shenfl/IdeaProjects/test/eshadoop/src/main/resources/people.json");
-//        person.show();
-//        JavaEsSparkSQL.saveToEs(person,"test/person");
-//    }
-//}
+package com.test.es.spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.elasticsearch.spark.sql.api.java.JavaEsSparkSQL;
+
+public class TestLocalFile2ES {
+    public static void main(String[] args) {
+        SparkSession spark=SparkSession.builder().appName("Sql2Es").config("es.nodes","172.17.41.193")
+                .config("es.port","9200").master("local[3]").getOrCreate();
+        // 没有es.mapping.id配置，则默认是es生成随机id
+        Dataset<Row> person=spark.read().json("/Users/shenfl/IdeaProjects/test/eshadoop/src/main/resources/people.json");
+        person.show();
+        JavaEsSparkSQL.saveToEs(person,"test/person");
+    }
+}

9dd440aa1f420716d7124db1eec6ffc4cfefa0eb
shenfl <shenfeilong@souche.com>
2020-06-10 16:11:09 +0800

    shenfeilong change kafka

--- common/src/main/java/com/test/kafka/Producer.java
+++ common/src/main/java/com/test/kafka/Producer.java
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
+import java.util.concurrent.Future;
 
 /**
  * Created by shenfl on 2018/5/25
@@ -21,17 +22,25 @@ public class Producer {
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
         String message = "world ";
-        for (int i = 0; i < 10; i++) { // 9
+        for (int i = 0; i < 1; i++) { // 9
             // 没有key，所以是轮流的方式往partition放
             // ("shenfl", "1", message + i) 如果固定了一个key，则数据都被放到一个分区中了，即使没有指定partitioner
-            producer.send(new ProducerRecord<String, String>("lulu", message + i), new Callback() {
+            int finalI = i;
+            Future<RecordMetadata> future = producer.send(new ProducerRecord<String, String>("test", message + i), new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    System.out.println(finalI);
                     System.out.println(recordMetadata.offset());
                     System.out.println(recordMetadata.partition());
                     System.out.println(recordMetadata.toString());
                 }
             });
+            try {
+                future.get();
+                System.out.println("aa");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         producer.close();
         System.out.println("over");

eb83ff53b0983942f2421b573a95fa0a50e2112c
shenfl <shenfeilong@souche.com>
2020-06-17 09:55:43 +0800

    shenfeilong

--- spring/src/main/java/com/test/spring/Welcomer.java
+++ spring/src/main/java/com/test/spring/Welcomer.java
@@ -1,14 +1,30 @@
 package com.test.spring;
 
-public class Welcomer {
+import org.springframework.beans.factory.InitializingBean;
+
+public class Welcomer implements InitializingBean {
 
     private MessageLocator message;
+    private String a = get();
+    private static String b = ggg();
+    static {
+        System.out.println("df");
+    }
 
     public Welcomer(){
         System.out.println("bb");
     }
+    private static String get() {
+        System.out.println("kk");
+        return "sds";
+    }
+    private static String ggg() {
+        System.out.println("ds");
+        return "s";
+    }
 
     public Welcomer(MessageLocator message) {
+        System.out.println("hhh");
         this.message = message;
     }
 
@@ -17,12 +33,18 @@ public class Welcomer {
     }
 
     public void setMessage(MessageLocator message) {
+        System.out.println("rrr");
         this.message = message;
     }
 
     public static Welcomer createWelcomer(MessageLocator messageLocator) {
         System.out.println("create welcomer");
         return new Welcomer(messageLocator);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
     }
 
     public static class MessageLocator{

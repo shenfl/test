137ff345a86d24a70b31ad7989c21b16114d31c3
shenfl <shenfeilong@souche.com>
2020-06-18 14:21:19 +0800

    shenfeilong add fastjson nested

--- spring/src/main/java/com/test/fastjson/Main.java
+++ spring/src/main/java/com/test/fastjson/Main.java
@@ -2,7 +2,15 @@ package com.test.fastjson;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.util.ParameterizedTypeImpl;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -28,5 +36,43 @@ public class Main {
                 "\t]\n" +
                 "}", Person.class);
         System.out.println(person);
+
+        Person p = work(Person.class);
+        System.out.println(p.toString());
+        // 当嵌套多层的时候
+        HashMap<String, ArrayList<HashMap<String, Person>>> result = workNested(Person.class);
+        System.out.println(result);
+    }
+
+    private static <T> T work(Class<T> tClass) {
+        HashMap<String, T> parseObject = JSON.parseObject("{\n" +
+                "\t\"_source\": {\n" +
+                "\t\t\"id\": 33,\n" +
+                "\t\t\"name\": \"shenfl\"\n" +
+                "\t}\n" +
+                "}", new TypeReference<HashMap<String, T>>(tClass) {
+        });
+        System.out.println(parseObject);
+        return parseObject.get("_source");
+    }
+    private static <T> T workNested(Class tClass) {
+        Type lastOne = new ParameterizedTypeImpl(new Type[]{String.class, tClass}, null, HashMap.class);
+        Type lastTwo = new ParameterizedTypeImpl(new Type[]{lastOne}, null, ArrayList.class);
+        Type lastThree = new ParameterizedTypeImpl(new Type[]{String.class, lastTwo}, null, HashMap.class);
+        HashMap<String, T> hashMap = JSON.parseObject(
+                "{\n" +
+                    "\t\"hits\": {\n" +
+                    "\t\t\"hits\": [\n" +
+                    "\t\t\t{\n" +
+                    "\t\t\t\t\"_source\": {\n" +
+                    "\t\t\t\t\t\"id\": 33,\n" +
+                    "\t\t\t\t\t\"name\": \"shenfl\"\n" +
+                    "\t\t\t\t}\n" +
+                    "\t\t\t}\n" +
+                    "\t\t]\n" +
+                    "\t}\n" +
+                    "}", new TypeReference<HashMap<String, T>>(lastThree) {
+        });
+        return hashMap.get("hits");
     }
 }

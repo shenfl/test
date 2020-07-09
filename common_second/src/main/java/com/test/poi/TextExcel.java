1fc24a51535b6d57b803f6d95cbb36b335e4086d
shenfl <shenfeilong@souche.com>
2020-07-09 10:00:07 +0800

    shenfeilong change excel

--- common_second/src/main/java/com/test/poi/TextExcel.java
+++ common_second/src/main/java/com/test/poi/TextExcel.java
@@ -1,5 +1,9 @@
 package com.test.poi;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -7,6 +11,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -15,7 +20,26 @@ import java.io.IOException;
  */
 public class TextExcel {
     public static void main(String[] args) throws Exception {
-        loadXlsx();
+//        loadXlsx();
+        writeXls();
+    }
+
+    private static void writeXls() throws IOException {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        //新建工作表
+        HSSFSheet sheet = workbook.createSheet("sheet1");
+        //创建行,行号作为参数传递给createRow()方法,第一行从0开始计算
+        HSSFRow row = sheet.createRow(0);
+        //创建单元格,row已经确定了行号,列号作为参数传递给createCell(),第一列从0开始计算
+        HSSFCell cell = row.createCell(2);
+        //设置单元格的值,即C1的值(第一行,第三列)
+        cell.setCellValue("hello sheet");
+        System.out.println(workbook.getBytes());
+        //输出到磁盘中
+        FileOutputStream fos = new FileOutputStream(new File("/Users/shenfl/work/aa.xls"));
+        workbook.write(fos);
+        workbook.close();
+        fos.close();
     }
 
     private static void loadXlsx() throws IOException {
